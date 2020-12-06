734b32e0ae4a85e57ee5f2f4a7df248600591bde
lailikumala92 <lailikumala92@gmail.com>
2020-12-06 11:09:05 -0800

    upload

--- src/Models/ChangePhoto.js
+++ src/Models/ChangePhoto.js
@@ -3,15 +3,11 @@ const photoModel = {
     
     photo: (params, body) => {
         const {id} = params;
+        const {photo} = body
         return new Promise((resolve, reject)=> {
             db.query(`SELECT photo FROM users WHERE id=${id}`, (err, res) => {
                 if(!err) {
-                // const data = Object.entries(body).map((item) => {
-                //     return parseInt(item[1]) > 0
-                //         ? `${item[0]}=${item[1]}`
-                //         : `${item[0]}='${item[1]}'`;
-                //     });
-                    db.query(`UPDATE users SET photo= '${body.photo}' WHERE id=${id}`, (err, res)=> {
+                    db.query(`UPDATE users SET photo = '${photo}' WHERE id=${id}`, (err, res)=> {
                         if(!err) {
                             resolve(res)
                         } else {
